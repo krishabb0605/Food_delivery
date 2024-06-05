e5c2b2e531bf26b3de85ef7f819b0847c6ef2cd0
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2024-06-05 12:55:37 +0530

    add order router

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 import 'dotenv/config';
 
 // App config
@@ -21,6 +22,7 @@ connectDB();
 app.use('/api/food', foodRouter);
 app.use('/api/user', userRouter);
 app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
 // for get image
 app.use('/images', express.static('uploads'));
