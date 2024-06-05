bb8c8746bc499ab0ab421782028b9829e463b243
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2024-06-05 11:58:41 +0530

    Cart data api

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
-import 'dotenv/config'
+import cartRouter from './routes/cartRoute.js';
+import 'dotenv/config';
 
 // App config
 const app = express();
@@ -19,6 +20,7 @@ connectDB();
 // API endpoint
 app.use('/api/food', foodRouter);
 app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter);
 
 // for get image
 app.use('/images', express.static('uploads'));
